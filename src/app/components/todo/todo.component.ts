940be249d7a41d17c669b6b92ee172ac804d69a1
Jeffry Gonzalez <jeff@hypertheory.com>
2020-05-20 15:51:48 -0400

    So Done!

---
+++ src/app/components/todo/todo.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoItem } from './models';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+
+  items: TodoItem[] = [
+    { description: 'Pull Weeds in Mulch', completed: true },
+    { description: 'Drink that beer', completed: false }
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addItem(descriptionEl: HTMLInputElement) {
+    this.items = [{ description: descriptionEl.value, completed: false }, ...this.items];
+    descriptionEl.value = '';
+    descriptionEl.focus();
+  }
+  markComplete(item: TodoItem) {
+    item.completed = true;
+  }
+
+  get hasCompletedItems() {
+    return this.items.some(item => item.completed);
+  }
+  removeCompletedItems() {
+    // your final exam. remove the completed items.
+    this.items = this.items.filter(item => !item.completed);
+  }
+}
